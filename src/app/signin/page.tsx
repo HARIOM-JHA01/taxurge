3911ec1bbce8780c7110d4562179561a38ac9cd1
HARIOM-JHA01 <jhahariom040@gmail.com>
2024-05-04 22:13:15 +0530

    change email to mobile no ui/fix

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -85,12 +85,12 @@ const SigninPage = () => {
                       htmlFor="email"
                       className="mb-3 block text-sm text-dark dark:text-white"
                     >
-                      Your Email
+                      Mobile number
                     </label>
                     <input
                       type="email"
                       name="email"
-                      placeholder="Enter your Email"
+                      placeholder="Enter your mobile no."
                       className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
                     />
                   </div>
