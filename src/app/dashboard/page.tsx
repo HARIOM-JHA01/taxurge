07e93e6d89b13500fb7e248339f35798115b319d
suman-mondal14 <sumanmondal142003@gmail.com>
2024-05-27 21:39:28 +0530

    new tab added in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -13,10 +13,12 @@ import {
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Input } from "@/components/ui/input";
 import clsx from "clsx";
+import Image from "next/image";
 
 const Dashboard = () => {
   const [files, setFiles] = useState<File[]>([]);
   const [activeTab, setActiveTab] = useState<string>("upload");
+  const [activeTab2, setActiveTab2] = useState<string>("payment");
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -47,13 +49,63 @@ const Dashboard = () => {
 
       <section className="pb-[190px] pt-[10px]">
         <div className="container">
-          <div className="flex w-full items-center justify-center">
+          <div className="flex w-full items-center justify-between">
+            <Tabs
+              defaultValue="payment"
+              className="w-[500px]"
+              onValueChange={(value) => setActiveTab2(value)}
+            >
+              <TabsList className="grid h-10 w-full grid-cols-2 border border-solid">
+                <TabsTrigger
+                  value="balance"
+                  className={clsx(
+                    activeTab2 === "balance" && "bg-[#002a7e] text-white",
+                  )}
+                >
+                  Balance
+                </TabsTrigger>
+                <TabsTrigger
+                  value="payment"
+                  className={clsx(
+                    activeTab2 === "payment" && "bg-[#002a7e] text-white",
+                  )}
+                >
+                  Payment
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="balance">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Balance</CardTitle>
+                  </CardHeader>
+                  <CardContent className="space-y-2">
+                    Remaining Balane : RS. 500
+                  </CardContent>
+                </Card>
+              </TabsContent>
+
+              <TabsContent value="payment">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Make Payment</CardTitle>
+                  </CardHeader>
+                  <CardContent className="space-y-2 flex justify-center items-start">
+                    <Image
+                      src="/images/payment/default_qrcode.png"
+                      width={100}
+                      height={100}
+                      alt="QR Code"
+                    />
+                  </CardContent>
+                </Card>
+              </TabsContent>
+            </Tabs>
             <Tabs
               defaultValue="upload"
               className="w-[500px]"
               onValueChange={(value) => setActiveTab(value)}
             >
-              <TabsList className="grid w-full grid-cols-2 border border-solid h-10">
+              <TabsList className="grid h-10 w-full grid-cols-2 border border-solid">
                 <TabsTrigger
                   value="upload"
                   className={clsx(
@@ -80,7 +132,7 @@ const Dashboard = () => {
                     {files.map((file, index) => (
                       <div
                         key={index}
-                        className="flex items-center justify-between border border-solid bg-[#002a7e] p-[0.3rem] text-white rounded-md"
+                        className="flex items-center justify-between rounded-md border border-solid bg-[#002a7e] p-[0.3rem] text-white"
                       >
                         <span>{file.name}</span>
                         <Button
